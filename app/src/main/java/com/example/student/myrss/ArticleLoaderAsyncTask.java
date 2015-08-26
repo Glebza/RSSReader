b815c81a20396e9f64ac40e092115b25bf008b13
Gleb <clockpen@gmail.com>
2015-08-26 22:37:57 +0300

    Work, but may be crushed

--- app/src/main/java/com/example/student/myrss/ArticleLoaderAsyncTask.java
+++ app/src/main/java/com/example/student/myrss/ArticleLoaderAsyncTask.java
@@ -1,13 +1,8 @@
 package com.example.student.myrss;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.view.TextureView;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -18,10 +13,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Глеб on 25-Aug-15.
@@ -37,34 +33,34 @@ public class ArticleLoaderAsyncTask extends AsyncTask<String,Void,Void> {
         bitmapImagesUrls = new ArrayList<>();
         markup = new LinkedHashMap<>();
     }
-/*TODO: Разобрать документ на составные части img и р. Потом положить все в map и затем вывести на экран в порядке добавления*/
+
     @Override
     protected Void doInBackground(String... params) {
         try {
-            Log.d(TAG, "in doInBackground to make a dirty");
 
             Document document = Jsoup.connect(params[0]).get();
             Element header = document.getElementsByTag("h1").first();
             articleHeader = header.text() ;
-            Log.d(TAG, articleHeader);
-            Elements images = document.getElementsByTag("section");
+
+            Elements sections = document.getElementsByTag("section");
+            Log.d(TAG, "Numbers of section: " + sections.size() + " " + sections.attr("class"));
+
             String imgSrc = "";
-            for (Element e: images){
-                for(Element element :e.children()){
-                    if (element.tag().equals("img")){
-                        markup.put(element.text(),"img");
-                    }else{
-                        if (element.tag().equals("")){
+            for (Element e: sections){
+                    Elements elements = e.getAllElements();
+               // Log.d(TAG, "Numbers of section's children: " + elements.size() + "");
+                    for (Element element : elements){
+                        Log.d(TAG,element.tag() + "");
+                        if (element.tag().toString().equals("p")){
                             markup.put(element.text(),"p");
                         }
-                    }
-                }
+                        if (element.tag().toString().equals("img")){
+                            Log.d(TAG,"im an image and im jump into map" + element.attr("src"));
+                            markup.put(element.attr("src"),"img");
 
-                //if (e.attr("alt").equals("User picture")){}
-                    imgSrc = e.attr("src");
-                    Log.d(TAG, imgSrc);
-                    //InputStream input = new java.net.URL(imgSrc).openStream();
-                    bitmapImagesUrls.add(imgSrc);
+                        }
+                    }
+
 
             }
             // Decode Bitmap
@@ -74,22 +70,37 @@ public class ArticleLoaderAsyncTask extends AsyncTask<String,Void,Void> {
         return null;
     }
 
+
     @Override
     protected void onPostExecute(Void aVoid) {
 
 
         TextView texView = new TextView(articleFragment.getActivity());
-       texView.setTextAppearance(articleFragment.getActivity().getApplicationContext(),R.style.headerText);
+        texView.setTextAppearance(articleFragment.getActivity().getApplicationContext(), R.style.headerText);
         articleFragment.getLinearLayout().addView(texView);
         texView.setText(articleHeader);
+        Iterator<Map.Entry<String, String>> itr1 = markup.entrySet().iterator();
+        while (itr1.hasNext()) {
+            Map.Entry<String, String> entry = itr1.next();
+            if (entry.getValue().equals("p")){
+                TextView textView = new TextView(articleFragment.getActivity());
+                texView.setTextAppearance(articleFragment.getActivity().getApplicationContext(), R.style.headerText);
+                articleFragment.getLinearLayout().addView(textView);
+                textView.setText(entry.getKey());
+            }  if (entry.getValue().equals("img")){
+                ImageView imageView = new ImageView(articleFragment.getActivity());
+                articleFragment.getLinearLayout().addView(imageView);
+                ImageLoader.getInstance().displayImage(entry.getKey(), imageView);
+            }
+
+        }
+
         // ImageView logo = (ImageView) articleFragment.getActivity().findViewById(R.id.newsPicture);
         for (String url : bitmapImagesUrls){
             if (articleFragment == null){
                 Log.d(TAG,"link to fragment is NUUUUUUUUUUUL bitch!");
             }
-            ImageView imageView = new ImageView(articleFragment.getActivity());
-            articleFragment.getLinearLayout().addView(imageView);
-            ImageLoader.getInstance().displayImage(url,imageView);
+
         }
     }
 
